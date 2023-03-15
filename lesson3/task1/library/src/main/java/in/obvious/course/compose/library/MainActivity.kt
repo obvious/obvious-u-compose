a4dac4169afe7d94af6e1fbec26924b8aef1805c
noyize <noyal@obvious.in>
2023-03-15 21:42:36 +0530

    Update module 1 - scrollable layouts task 1: Scrollable Column

--- lesson3/task1/library/src/main/java/in/obvious/course/compose/library/MainActivity.kt
+++ lesson3/task1/library/src/main/java/in/obvious/course/compose/library/MainActivity.kt
@@ -3,26 +3,14 @@ package `in`.obvious.course.compose.library
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import `in`.obvious.course.compose.library.theme.ComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
