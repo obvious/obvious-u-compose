218f35588671eecdcadfa14a6d90f2185b590dad
noyize <noyal@obvious.in>
2023-04-06 09:33:04 +0530

    Update module 3 - animation spec task 4: Repeating

---
+++ lesson13/task4/src/main/java/in/obvious/course/compose/MainActivity.kt
@@ -0,0 +1,76 @@
+package `in`.obvious.course.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.EaseOutExpo
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.keyframes
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import `in`.obvious.course.compose.theme.ComposePlaygroundTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposePlaygroundTheme {
+                Fan()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Fan() {
+    var isRotated by remember { mutableStateOf(false) }
+    val rotationAngle by animateFloatAsState(
+        targetValue = if (isRotated) 360F else 0f,
+        label = "rotation",
+        animationSpec = keyframes {
+            durationMillis = 4000
+            150f at 1000
+            340f at 2000 with EaseOutExpo
+
+        }
+    )
+
+    val rotationColor = if (rotationAngle == 360F) Color.Red else Color.DarkGray
+
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.fan),
+            contentDescription = "fan",
+            modifier = Modifier
+                .rotate(rotationAngle)
+                .size(150.dp),
+            tint = rotationColor
+
+        )
+
+        Button(
+            onClick = { isRotated = !isRotated },
+            modifier = Modifier
+                .padding(top = 52.dp)
+        ) {
+            Text(text = "Rotate Fan")
+        }
+    }
+}
---
+++ lesson13/task4/task.md
@@ -0,0 +1,5 @@
+For the `rotationAngle` in `Fan` composable set the animation spec to repeat `tween` animation of duration 2 seconds for 3 times 
+
+__
+
+<img src="assets/ezgif-5-4200501c32.gif"  width="350" height="720"/>
